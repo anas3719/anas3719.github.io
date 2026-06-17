cd741fe95c06d3d84b5619f0f6d2caaa8e54f663
Anas3719 <omaranas3719@gmail.com>
2026-06-17 22:19:15 +0300

    Use model photo for Mahmoud photographer

--- photographers-data.js
+++ photographers-data.js
@@ -3,6 +3,6 @@ window.photographers = [
     id: "mahmoud-alshabrawi",
     name: "المصور محمود الشبراوي",
     folderUrl: "https://drive.google.com/drive/folders/1xStPZz7nUZFLB1y8qppys0u2_8R7a2kY",
-    photoUrl: "",
+    photoUrl: "https://drive.google.com/thumbnail?id=1EuLgomQvRnTDSpi22MscSaAFWbjoRcWj&sz=w1000",
   },
 ];
